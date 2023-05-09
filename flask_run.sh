95b24550274c8a6d3891f32a49812217d60b3da8
Jung Jonghoon <jonghoon93@gmail.com>
2023-05-09 19:31:50 +0900

    mac modify

--- flask_run.sh
+++ flask_run.sh
@@ -1,9 +1,10 @@
 export FLASK_APP=web.py
 export FLASK_ENV=development
+export FLASK_RUN_PORT=5200
 
 # foreground run
-#python -m flask run
-python -m flask run --host=127.0.0.1 --port=5001
+python3 -m flask run
+#python -m flask run --host=127.0.0.1 --port=5001
 
 # background run
 # nohup /usr/bin/python3.8 -m flask run --host=127.0.0.1 --port=5000 &
