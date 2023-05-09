00a9bfe8338cc80a5064e8bf1b4389501d965774
Jung Jonghoon <jonghoon93@gmail.com>
2023-05-09 23:21:24 +0900

    shell modify

--- flask_run.sh
+++ flask_run.sh
@@ -2,9 +2,13 @@ export FLASK_APP=web.py
 export FLASK_ENV=development
 export FLASK_RUN_PORT=5200
 
+cd /Users/jonghoon/DATA/coding/python/topsolar/
+
 # foreground run
-nohup python3 -m flask run --no-debugger &
-#python -m flask run --host=127.0.0.1 --port=5001
+#python3 -m flask run --no-debugger --port 5200
 
 # background run
+nohup python3 -m flask run --no-debugger --port 5200 &
+
+#python -m flask run --host=127.0.0.1 --port=5001
 # nohup /usr/bin/python3.8 -m flask run --host=127.0.0.1 --port=5000 &
